90f56748460ef392d922092b85695a89957c65bb
katana <you@example.com>
2026-03-08 14:32:34 +0000

    3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,9 +3,11 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse_lazy
 from .models import Book
 from .models import Library
+from .models import UserProfile
 
 
 def list_books(request):
@@ -38,3 +40,42 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+
+
+def is_admin(user):
+    return (
+        user.is_authenticated
+        and hasattr(user, "userprofile")
+        and user.userprofile.role == "Admin"
+    )
+
+
+def is_librarian(user):
+    return (
+        user.is_authenticated
+        and hasattr(user, "userprofile")
+        and user.userprofile.role == "Librarian"
+    )
+
+
+def is_member(user):
+    return (
+        user.is_authenticated
+        and hasattr(user, "userprofile")
+        and user.userprofile.role == "Member"
+    )
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
